e483603e2a8f6d6e4c33be920ae4c5b7f1b7c2e6
Akvotis <shef01.00@gmail.com>
2022-11-02 18:07:34 +0500

    подготовка к запуску github pages

--- src/scripts/modules/tabsToggle.js
+++ src/scripts/modules/tabsToggle.js
@@ -1,38 +1,37 @@
-export const tabsToggle = (tabsSelector, tabsHeadSelector, tabsBodySelector, tabsCaptionSelector, tabsCaptionActiveClass, tabsContentActiveClass) => { // объявляем основную функцию tabs, которая будет принимать CSS классы и селекторы
+export const tabsToggle = (tabsSelector, tabsHeadSelector, tabsBodySelector, tabsCaptionSelector, tabsCaptionActiveClass, tabsContentActiveClass) => {
 
     const tabs = document.querySelector(tabsSelector);
-    const head = tabs.querySelector(tabsHeadSelector) // ищем в элементе tabs элемент с кнопками по переданному селектору и записываем в константу
-    const body = tabs.querySelector(tabsBodySelector) // ищем в элементе tabs элемент с контентом по переданному селектору и записываем в константу
+    const head = tabs.querySelector(tabsHeadSelector);
+    const body = tabs.querySelector(tabsBodySelector);
 
-    const getActiveTabName = () => { // функция для получения названия активной вкладки
-        return head.querySelector(`.${tabsCaptionActiveClass}`).dataset.tab // возвращаем значение data-tab активной кнопки
+    const getActiveTabName = () => {
+        return head.querySelector(`.${tabsCaptionActiveClass}`).dataset.tab;
     }
 
-    const setActiveContent = () => { // функция для установки активного элемента контента
-        if (body.querySelector(`.${tabsContentActiveClass}`)) { // если уже есть активный элемент контента
-            body.querySelector(`.${tabsContentActiveClass}`).classList.remove(tabsContentActiveClass) // то скрываем его
+    const setActiveContent = () => {
+        if (body.querySelector(`.${tabsContentActiveClass}`)) {
+            body.querySelector(`.${tabsContentActiveClass}`).classList.remove(tabsContentActiveClass);
         }
-        body.querySelector(`[data-tab=${getActiveTabName()}]`).classList.add(tabsContentActiveClass) // затем ищем элемент контента, у которого значение data-tab совпадает со значением data-tab активной кнопки и отображаем его
+        body.querySelector(`[data-tab=${getActiveTabName()}]`).classList.add(tabsContentActiveClass);
     }
 
     // проверяем при загрузке страницы, есть ли активная вкладка
-    if (!head.querySelector(`.${tabsCaptionActiveClass}`)) { // если активной вкладки нет
-        head.querySelector(tabsCaptionSelector).classList.add(tabsCaptionActiveClass) // то делаем активной по-умолчанию первую вкладку
+    if (!head.querySelector(`.${tabsCaptionActiveClass}`)) {
+        head.querySelector(tabsCaptionSelector).classList.add(tabsCaptionActiveClass);
     }
 
-    setActiveContent(getActiveTabName()) // устанавливаем активный элемент контента в соответствии с активной кнопкой при загрузке страницы
+    setActiveContent(getActiveTabName());
 
-    head.addEventListener('click', e => { // при клике на элемент с кнопками
-        const caption = e.target.closest(tabsCaptionSelector) // узнаем, был ли клик на кнопке
-        if (!caption) return // если клик был не на кнопке, то прерываем выполнение функции
-        if (caption.classList.contains(tabsCaptionActiveClass)) return // если клик был на активной кнопке, то тоже прерываем выполнение функции и ничего не делаем
-
-        if (head.querySelector(`.${tabsCaptionActiveClass}`)) { // если уже есть активная кнопка
-            head.querySelector(`.${tabsCaptionActiveClass}`).classList.remove(tabsCaptionActiveClass) // то удаляем ей активный класс
+    head.addEventListener('click', e => {
+        const caption = e.target.closest(tabsCaptionSelector);
+        if (!caption) return;
+        if (caption.classList.contains(tabsCaptionActiveClass)) return;
+        if (head.querySelector(`.${tabsCaptionActiveClass}`)) {
+            head.querySelector(`.${tabsCaptionActiveClass}`).classList.remove(tabsCaptionActiveClass);
         }
 
-        caption.classList.add(tabsCaptionActiveClass) // затем добавляем активный класс кнопке, на которой был клик
+        caption.classList.add(tabsCaptionActiveClass);
 
-        setActiveContent(getActiveTabName()) // устанавливаем активный элемент контента в соответствии с активной кнопкой
+        setActiveContent(getActiveTabName());
     })
 }
